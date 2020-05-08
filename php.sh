126ecc5074f59b79fd9e2e41e47bf670b3005466
54maxwu <30310328+54maxwu@users.noreply.github.com>
2020-05-08 11:57:31 +0800

    Update and rename php安装.md to php.sh

--- php.sh
+++ php.sh
@@ -1,8 +1,5 @@
-__安装php的shell script__
-```sh
 #!/bin/bash
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sed -i -e '9c enabled=1' /etc/yum.repos.d/remi-php72.repo
 yum install -y php
-```
