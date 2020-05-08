ddd8c098dea562a5d632901fb5cfd7f42ffd996d
54maxwu <30310328+54maxwu@users.noreply.github.com>
2020-05-08 11:55:51 +0800

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,5 +1,7 @@
+```sh
 #!/bin/bash
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sed -i -e '9c enabled=1' /etc/yum.repos.d/remi-php72.repo
 yum install -y php
+```
